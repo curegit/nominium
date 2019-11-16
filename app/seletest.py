749facd21fb1c9a1648f82f678bfaaf12eb04fe8
Cure Git <37978051+curegit@users.noreply.github.com>
2019-11-17 00:41:59 +0900

    Revert "Update test"
    This reverts commit 81ad66e622072b6f87951607d981717e62c5840c.

--- app/seletest.py
+++ app/seletest.py
@@ -25,6 +25,9 @@ options.add_argument("--disable-gpu")
 options.add_argument("--disable-desktop-notifications")
 options.add_argument("--disable-extensions")
 
+# 画像を読まない
+options.add_argument("--blink-settings=imagesEnabled=false")
+
 # エラーの許容
 options.add_argument("--ignore-certificate-errors")
 options.add_argument("--allow-running-insecure-content")
