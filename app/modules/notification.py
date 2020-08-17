0dfc413fc61eff2e8850037c3130e6479888e49d
curegit <37978051+curegit@users.noreply.github.com>
2020-08-18 00:35:10 +0900

    Update notification

--- app/modules/notification.py
+++ app/modules/notification.py
@@ -1,3 +1,4 @@
+import datetime
 from smtplib import SMTP_SSL
 from email.utils import formatdate
 from email.mime.text import MIMEText
@@ -26,3 +27,22 @@ def smtp_send(host, port, user, password, mailfrom, mailto, messages):
 def send(mails):
 	messages = [create_message(mail_from, mail_to, mail_bcc, subject, body) for subject, body in mails]
 	smtp_send(smtp_host, smtp_port, smtp_user, smtp_passwd, mail_from, mail_to, messages)
+
+# 通知配信を制限に則って行うクラス
+class NotificationController():
+
+	# コンストラクタ
+	def __init__(self, max_per_hour):
+		self.count = 0
+		self.hour = datetime.datetime.now().strftime("%Y-%m-%d %H")
+		self.max_per_hour = max_per_hour
+
+	# 複数のメールを設定に基づいて時間あたりの最大件数を超えないように送信する
+	def send(self, mails):
+		hour = datetime.datetime.now().strftime("%Y-%m-%d %H")
+		if self.hour != hour:
+			self.hour = hour
+			self.count = 0
+		num = min(len(mails), self.max_per_hour - self.count)
+		self.count += num
+		send(mails[0:num])
