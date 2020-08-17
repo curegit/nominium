a83a1fc2946acd059b017bad771750e35bd1cd71
curegit <37978051+curegit@users.noreply.github.com>
2020-08-17 15:38:22 +0900

    Update logging

--- app/modules/logging.py
+++ app/modules/logging.py
@@ -1,35 +1,53 @@
 import datetime
+from queue import Queue
 from modules.utilities import file_path, rel_path
 
-# ログに書く時刻情報の文字列を返す
-def time_string():
-	return datetime.datetime.now().strftime("[%Y-%m-%d %H:%M:%S]")
+# ログを保存するディレクトリ
+log_dir = rel_path("../../logs")
 
-# 現在の日付に応じてログファイルのパスを返す
-def log_file_path():
-	log_dir = rel_path("../../logs")
-	log_name = datetime.date.today().strftime("%Y-%m-%d")
+# 日付に応じてログファイルのパスを返す
+def log_file_path(dtime):
+	log_name = dtime.strftime("%Y-%m-%d")
 	return file_path(log_dir, log_name, "log")
 
-# 今日のログに1行追記する
-def log_line(str):
-	with open(log_file_path(), mode="a", encoding="utf-8") as file:
-		file.write(f"{time_string()} {str}\n")
+# ログに書く時刻情報の文字列を返す
+def time_string(dtime):
+	return dtime.strftime("[%Y-%m-%d %H:%M:%S]")
 
-# ログに発生した例外情報を追記する
-def log_exception(exception):
-	log_line(str(exception))
+# ログの行フォーマットを施した文字列を返す
+def log_line_format(dtime, message):
+	return f"{time_string(dtime)} {message}\n"
 
-# 今日のログに1行追記する（失敗しても例外を送出しない）
-def try_log_line(str):
-	try:
-		log_line(str)
-	except:
-		pass
+# 複数スレッドから集約的に書き込むためのロガー
+class Logger():
 
-# ログに発生した例外情報を追記する（失敗しても例外を送出しない）
-def try_log_line(exception):
-	try:
-		log_exception(exception)
-	except:
-		pass
+	# コンストラクタ
+	def __init__(self):
+		self.queue = Queue()
+
+	# ログキューに1行追記する
+	def log_line(message):
+		self.queue.put((datetime.datetime.now(), message))
+
+	# ログキューに発生した例外情報を追記する
+	def log_exception(exception):
+		log_line(str(exception))
+
+	# キューにあるログをファイルに書き込む（非スレッドセーフ）
+	def commit():
+		qsize = self.queue.qsize
+		if qsize > 0:
+			dtime, message = self.queue.get()
+			log_path = log_file_path(dtime)
+			pending = qsize
+			while pending > 0:
+				with open(log_path, mode="a", encoding="utf-8") as file:
+					file.write(log_line_format(dtime, message))
+					pending -= 1
+					while pending > 0:
+						dtime, message = self.queue.get()
+						if log_file_path(dtime) != log_path:
+							log_path = log_file_path(dtime)
+							break
+						file.write(log_line_format(dtime, message))
+						pending -= 1
