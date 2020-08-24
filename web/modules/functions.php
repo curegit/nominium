dc1745f415c6c12db702eb014716900c6cf823c9
curegit <37978051+curegit@users.noreply.github.com>
2020-08-24 22:09:38 +0900

    Fix db

--- web/modules/functions.php
+++ web/modules/functions.php
@@ -6,7 +6,7 @@ function h($html) {
 
 // データベースへの接続を開いて返す
 function open_db() {
-  $db_path = __DIR__."/../../app/ziraffem.db";
+  $db_path = realpath(__DIR__."/../../data/nominium.db");
   $pdo = new PDO("sqlite:".$db_path);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
