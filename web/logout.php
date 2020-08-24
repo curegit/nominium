9cefb4f78b8de8b368314c8a8a03c2931c83c7dd
curegit <37978051+curegit@users.noreply.github.com>
2020-08-24 22:15:57 +0900

    Update logout.php

--- web/logout.php
+++ web/logout.php
@@ -5,9 +5,9 @@ header("HTTP/1.1 401 Unauthorized");
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Logged Out</title>
+    <title>ログアウト</title>
   </head>
   <body>
-    <p>You've been logged out successfully.</p>
+    <p>ログアウトしました。</p>
   </body>
 </html>
