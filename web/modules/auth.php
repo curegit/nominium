728fbcc78b0158dbb4a9069ba71d93a6e30bcaf7
curegit <37978051+curegit@users.noreply.github.com>
2020-08-24 21:34:52 +0900

    Renew auth module

---
+++ web/modules/auth.php
@@ -0,0 +1,13 @@
+<?php
+$ini_path = realpath(__DIR__."/../../conf/settings.ini");
+$ini_array = parse_ini_file($ini_path, true, INI_SCANNER_RAW);
+$user = $ini_array["web"]["user"];
+$password = $ini_array["web"]["password"];
+switch (true) {
+  case !isset($_SERVER["PHP_AUTH_USER"], $_SERVER["PHP_AUTH_PW"]):
+  case $_SERVER["PHP_AUTH_USER"] !== $user:
+  case $_SERVER["PHP_AUTH_PW"] !== $password:
+    header("WWW-Authenticate: Basic realm=\"Nominium\"");
+    header("Content-Type: text/plain; charset=utf-8");
+    die("ログインしてください。".PHP_EOL);
+}
