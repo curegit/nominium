cbb08ce872e2255f965682a88d30e5826cf58a5b
curegit <37978051+curegit@users.noreply.github.com>
2020-08-27 17:46:21 +0900

    Add test script

---
+++ app/test.py
@@ -0,0 +1,10 @@
+from modules.crawling import init_driver
+from modules.notification import send
+
+# WebDriverのテスト
+driver = init_driver()
+driver.get("http://example.com/")
+driver.quit()
+
+# メール送信のテスト
+send([("テストメール", "テストメール")])
