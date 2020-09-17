4609fce45c82820708e499989d8daed5ddf73720
curegit <37978051+curegit@users.noreply.github.com>
2020-09-17 14:55:32 +0900

    Update mercari

--- app/plugins/sites/mercari.py
+++ app/plugins/sites/mercari.py
@@ -3,22 +3,20 @@ from urllib.parse import urlparse
 from urllib.parse import urlencode
 from bs4 import BeautifulSoup
 
-#
+# サイトの識別子
 name = "Mercari"
 
-#
+# フェッチャーにさせる動作
 def get(driver, keyword):
 	query = { "keyword": keyword, "sort_order": "created_desc", "status_on_sale": "1" }
 	driver.get(f"https://www.mercari.com/jp/search/?{urlencode(query)}")
-	if len(driver.find_elements_by_class_name("search-result-description")) > 0:
-		return []
-	items = driver.find_elements_by_class_name("items-box-container")
-	return [i.get_attribute("outerHTML") for i in items]
+	container = driver.find_element_by_class_name("items-box-container")
+	return container.get_attribute("outerHTML")
 
-# イテレータ
+# フェッチしたデータの処理
 def extract(documents):
-	for document in documents:
-		bs = BeautifulSoup(document, "html.parser")
+	bs = BeautifulSoup(documents, "html.parser")
+	if not bs.select_one(".search-result-description"):
 		for b in bs.select(".items-box"):
 			path = urlparse(b.select_one("a")["href"]).path
 			id = re.search("/(m[0-9]+)/", path).group(1)
@@ -26,5 +24,6 @@ def extract(documents):
 			title = b.select_one("h3").get_text(strip=True)
 			img_url = urlparse(b.select_one("img")["data-src"])
 			img = img_url.scheme + "://" + img_url.netloc + img_url.path
-			price = int("".join([d for d in b.select_one(".items-box-price").get_text(strip=True) if d != "¥" and d != ","]))
+			price_str = b.select_one(".items-box-price").get_text(strip=True)
+			price = int("".join([c for c in price_str if c in [str(i) for i in range(10)]]))
 			yield (id, url, title, img, price)
