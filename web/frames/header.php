b0d810d644a1cc82a1d47c4371041c35aa3a8e11
curegit <37978051+curegit@users.noreply.github.com>
2020-09-21 00:55:39 +0900

    Update style

--- web/frames/header.php
+++ web/frames/header.php
@@ -8,12 +8,12 @@
   </head>
   <body>
     <header>
-      <h1>Nominium</h1>
+      <h1>Nominium – <?= h(PAGE_TITLE) ?></h1>
       <nav class="menu">
         <ul>
           <li><a href="./">ホーム</a></li>
-          <li><a href="./register.php">キーワード登録</a></li>
-          <li><a href="./delete.php">キーワード削除</a></li>
+          <li><a href="./register.php">登録</a></li>
+          <li><a href="./delete.php">削除</a></li>
           <li><a href="./delete.php">重要度</a></li>
           <li><a href="./filter.php">フィルタ</a></li>
           <li><a href="./inspect.php">監査</a></li>
