3ccdfc6f2491b8fb042a00af879899a961dbf9e4
curegit <37978051+curegit@users.noreply.github.com>
2020-09-21 00:55:26 +0900

    Fix log

--- web/modules/functions.php
+++ web/modules/functions.php
@@ -19,7 +19,7 @@ function get_today_log() {
   $date = date("Y-m-d");
   $log_path = realpath(__DIR__."/../../logs/{$date}.log");
   if (file_exists($log_path) && filesize($log_path) > 0) {
-    return file_get_contents($path);
+    return file_get_contents($log_path);
   } else {
     return false;
   }
