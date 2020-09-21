0973ce562d20916f57ca259013070ee901aad642
curegit <37978051+curegit@users.noreply.github.com>
2020-09-21 23:16:16 +0900

    Change param name

--- web/modules/functions.php
+++ web/modules/functions.php
@@ -1,7 +1,7 @@
 <?php
 // HTMLメタ文字のエスケープ
-function h($html) {
-  return htmlspecialchars($html, ENT_QUOTES, "UTF-8");
+function h($str) {
+  return htmlspecialchars($str, ENT_QUOTES, "UTF-8");
 }
 
 // データベースへの接続を開いて返す
