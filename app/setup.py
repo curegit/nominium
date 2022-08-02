d4ff7cf2f4a3f31d4960a684ef5210b502e9f69e
curegit <37978051+curegit@users.noreply.github.com>
2022-08-02 22:07:47 +0900

    セットアップ時にDB初期化を行う

--- app/setup.py
+++ app/setup.py
@@ -15,6 +15,10 @@ mkdirp(data_dir)
 # データベース作成
 with connect() as connection:
 	cursor = connection.cursor()
+	cursor.execute("DROP TABLE IF EXISTS keyword")
+	cursor.execute("DROP TABLE IF EXISTS filter")
+	cursor.execute("DROP TABLE IF EXISTS history")
+	cursor.execute("DROP TABLE IF EXISTS item")
 	cursor.execute("CREATE TABLE IF NOT EXISTS keyword(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, keyword TEXT NOT NULL UNIQUE, importance REAL NOT NULL, priority INTEGER NOT NULL DEFAULT 0, CHECK(importance > 0.0 AND importance <= 1.0))")
 	cursor.execute("CREATE TABLE IF NOT EXISTS filter(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, pattern TEXT NOT NULL UNIQUE)")
 	cursor.execute("CREATE TABLE IF NOT EXISTS history(site TEXT NOT NULL, keyword INTEGER NOT NULL, UNIQUE(site, keyword))")
