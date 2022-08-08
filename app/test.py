2d7d9f4616d6a20458cd15e816256c86f9bc6b46
curegit <37978051+curegit@users.noreply.github.com>
2022-08-08 17:24:41 +0900

    テストスクリプトを更新

--- app/test.py
+++ app/test.py
@@ -1,11 +1,38 @@
+from time import sleep
+from modules.config import mail_enabled
 from modules.crawling import init_driver
 from modules.notification import send
 
 # WebDriverのテスト
-driver = init_driver()
-driver.get("https://example.com/")
-driver.quit()
+try:
+	print("WebDriver をテストします")
+	driver = init_driver()
+	driver.get("https://example.com/")
+	sleep(5)
+except:
+	print("example.com の取得に失敗しました")
+	print("WebDriver の設定または環境に問題があります")
+	print("テストを中断します")
+	raise
+else:
+	print("example.com の取得に成功しました")
+	print("WebDriver のエラーは観測されませんでした")
+finally:
+	driver.quit()
 
 # メール送信のテスト
 if mail_enabled:
-	send([("テストメール", "Nominium テストメールです。", "<html><head><title>テストメール</title></head><body><p>Nominium テストメールです。</p></body></html>")])
+	print("SMTP 構成をテストします")
+	try:
+		send([("テストメール", "Nominium テストメールです。", "<html><head><title>テストメール</title></head><body><p>Nominium テストメールです。</p></body></html>")])
+	except:
+		print("メールを送信できませんでした")
+		print("SMTP の構成または環境に問題があります")
+		print("テストを中断します")
+		raise
+	else:
+		print("メールを送信しました")
+else:
+	print("メール通知が無効なので SMTP テストを実施しません")
+
+print("テストは完了しました")
