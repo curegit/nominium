01af3bf496df45ccd50cb9279e6bcbe266207a2d
curegit <37978051+curegit@users.noreply.github.com>
2022-11-23 23:28:59 +0900

    appenditems

--- web/assets/refresh.js
+++ web/assets/refresh.js
@@ -4,6 +4,31 @@ var intid = null;
 //t.checked = true;
 //s.selectedIndex = 0;
 
+function appenditems(newdocument) {
+  const newdivs = newdocument.getElementsByClassName("items");
+  if (!newdivs.length) return;
+  const newdiv = newdivs[0];
+  const divs = document.getElementsByClassName("nothing");
+  if (divs.length) {
+    const div = divs[0];
+    div.insertAdjacentElement("beforebegin", newdiv);
+    div.remove();
+  } else {
+    const divs = document.getElementsByClassName("items");
+    if (!divs.length) return;
+    const div = divs[0];
+    const incomings = [];
+    let hrefs = [...div.children].map(e => e.getElementsByTagName("a")[0].href);
+    for (let i = 0; i < newdiv.children.length; i++) {
+      let href = newdiv.children[i].getElementsByTagName("a")[0].href;
+      if (!hrefs.includes(href)) {
+        incomings.push(newdiv.children[i]);
+      }
+    }
+    div.prepend(...incomings);
+  }
+}
+
 function refresh() {
   const xhr = new XMLHttpRequest();
   xhr.open("GET", "./");
@@ -11,27 +36,9 @@ function refresh() {
   xhr.timeout = 10000;
   xhr.onload = event => {
     if (xhr.status === 200) {
-      const d = xhr.response.getElementsByClassName("items");
-      if (d.length < 1) { return; }
-      const div2 = d[0];
-      const ndiv = document.getElementsByClassName("nothing");
-      if (ndiv.length >= 1) {
-        ndiv[0].insertAdjacentElement("beforebegin", div2);
-        ndiv[0].remove();
-      }
-      let div1 = document.getElementsByClassName("items")[0];
-
-      let hrefs = [...div1.children].map(e => e.getElementsByTagName("a")[0].href);
-      let news = [];
-      for (let i = 0; i < div2.children.length; i++) {
-        let href = div2.children[i].getElementsByTagName("a")[0].href;
-        if (!hrefs.includes(href)) {
-          news.push(div2.children[i]);
-        }
-      }
-      div1.prepend(...news);
+      const doc = xhr.response;
+      appenditems(doc);
     } else {
-
     }
   };
   xhr.onerror = event => {
