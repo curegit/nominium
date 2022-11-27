2395a80f2f5299bd777ca4568351165a6fbded36
curegit <37978051+curegit@users.noreply.github.com>
2022-11-27 23:24:04 +0900

    Use addEventListener

--- web/assets/reorder.js
+++ web/assets/reorder.js
@@ -1,68 +1,84 @@
-var dragsource = null;
+(function () {
+  let dragsource = null;
 
-function dragstart(event) {
-  dragsource = event.currentTarget;
-  event.dataTransfer.effectAllowed = "move";
-  const children = dragsource.parentElement.children;
-  for (let i = 0, above = true; i < children.length; i++) {
-    if (children[i] === dragsource) {
-      above = false;
-      children[i].classList.remove("above", "below");
+  function dragstart(event) {
+    dragsource = event.currentTarget;
+    event.dataTransfer.effectAllowed = "move";
+    const children = dragsource.parentElement.children;
+    for (let i = 0, above = true; i < children.length; i++) {
+      if (children[i] === dragsource) {
+        above = false;
+        children[i].classList.remove("above", "below");
+      } else {
+        if (above) {
+          children[i].classList.add("above");
+          children[i].classList.remove("below");
+        } else {
+          children[i].classList.add("below");
+          children[i].classList.remove("above");
+        }
+      }
+    }
+  }
+
+  function dragend(event) {
+    const children = event.currentTarget.parentElement.children;
+    for (let i = 0; i < children.length; i++) {
+      children[i].classList.remove("over", "above", "below");
+    }
+  }
+
+  function dragover(event) {
+    event.preventDefault();
+    if (event.currentTarget !== dragsource) {
+      event.dataTransfer.dropEffect = "move";
+      event.currentTarget.classList.add("over");
     } else {
-      if (above) {
-        children[i].classList.add("above");
-        children[i].classList.remove("below");
-      } else {
-        children[i].classList.add("below");
-        children[i].classList.remove("above");
-      }
+      event.dataTransfer.dropEffect = "none";
     }
   }
-}
 
-function dragend(event) {
-  const children = event.currentTarget.parentElement.children;
-  for (let i = 0; i < children.length; i++) {
-    children[i].classList.remove("over", "above", "below");
+  function dragleave(event) {
+    event.currentTarget.classList.remove("over");
   }
-}
 
-function dragover(event) {
-  event.preventDefault();
-  if (event.currentTarget !== dragsource) {
-    event.dataTransfer.dropEffect = "move";
-    event.currentTarget.classList.add("over");
-  } else {
-    event.dataTransfer.dropEffect = "none";
-  }
-}
-
-function dragleave(event) {
-  event.currentTarget.classList.remove("over");
-}
-
-function drop(event) {
-  event.preventDefault();
-  event.stopPropagation();
-  const target = event.currentTarget;
-  const parent = target.parentElement;
-  const children = [...parent.children];
-  for (let i = 0; i < children.length; i++) {
-    if (children[i] === target) {
-      if (children[i].classList.contains("above")) {
-        parent.appendChild(dragsource);
-        parent.appendChild(children[i]);
-      } else if (children[i].classList.contains("below")) {
-        parent.appendChild(children[i]);
-        parent.appendChild(dragsource);
-      } else {
+  function drop(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    const target = event.currentTarget;
+    const parent = target.parentElement;
+    const children = [...parent.children];
+    for (let i = 0; i < children.length; i++) {
+      if (children[i] === target) {
+        if (children[i].classList.contains("above")) {
+          parent.appendChild(dragsource);
+          parent.appendChild(children[i]);
+        } else if (children[i].classList.contains("below")) {
+          parent.appendChild(children[i]);
+          parent.appendChild(dragsource);
+        } else {
+          parent.appendChild(children[i]);
+        }
+      } else if (children[i] !== dragsource) {
         parent.appendChild(children[i]);
       }
-    } else if (children[i] !== dragsource) {
-      parent.appendChild(children[i]);
+    }
+    for (let i = 0; i < children.length; i++) {
+      children[i].classList.remove("over", "above", "below");
     }
   }
-  for (let i = 0; i < children.length; i++) {
-    children[i].classList.remove("over", "above", "below");
+
+  const reorderables = document.getElementsByClassName("reorderable");
+  for (let i = 0; i < reorderables.length; i++) {
+    const reorderable = reorderables[i];
+    const items = reorderable.children;
+    for (let j = 0; j < items.length; j++) {
+      const item = items[j];
+      item.addEventListener("dragstart", dragstart);
+      item.addEventListener("dragend", dragend);
+      item.addEventListener("dragover", dragover);
+      item.addEventListener("dragleave", dragleave);
+      item.addEventListener("drop", drop);
+    }
   }
-}
+})();
