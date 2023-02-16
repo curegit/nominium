35800b03e037aa89701fca510a5fd1faac81f898
curegit <37978051+curegit@users.noreply.github.com>
2023-02-16 23:57:56 +0900

    ログアウト操作をPOST専用にする

--- web/logout.php
+++ web/logout.php
@@ -1,8 +1,14 @@
 <?php
-http_response_code(401);
+require_once "./modules/auth.php";
+require_once "./modules/csrf.php";
+require_once "./modules/functions.php";
+
+$logout = $_SERVER["REQUEST_METHOD"] === "POST";
 ?>
+<?php IF($logout): ?>
+<?php http_response_code(401); ?>
 <!DOCTYPE html>
-<html>
+<html lang="ja">
   <head>
     <meta charset="utf-8">
     <title>ログアウト</title>
@@ -11,3 +17,16 @@ http_response_code(401);
     <p>ログアウトを要求しました。</p>
   </body>
 </html>
+<?php ELSE: ?>
+<?php define("PAGE_TITLE", "ログアウト"); ?>
+<?php include "./frames/header.php"; ?>
+    <main>
+      <section>
+        <h2>ログアウト要求</h2>
+        <form method="post">
+          <input type="submit" value="ログアウトする">
+        </form>
+      </section>
+    </main>
+<?php include "./frames/footer.php"; ?>
+<?php ENDIF; ?>
