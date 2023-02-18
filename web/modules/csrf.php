0965b981e413a226e7d38de8c7f2a5a2dac752e7
curegit <37978051+curegit@users.noreply.github.com>
2023-02-18 18:30:56 +0900

    sec-fetch がないときは許可

--- web/modules/csrf.php
+++ web/modules/csrf.php
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] !== "GET" && $_SERVER["REQUEST_METHOD"] !== "HEAD") {
-  if (!isset($_SERVER["HTTP_SEC_FETCH_SITE"]) || strtolower($_SERVER["HTTP_SEC_FETCH_SITE"]) !== "same-origin") {
+  if (isset($_SERVER["HTTP_SEC_FETCH_SITE"]) && strtolower($_SERVER["HTTP_SEC_FETCH_SITE"]) !== "same-origin") {
     http_response_code(400);
     header("Content-Type: text/plain; charset=utf-8");
     die("不正な遷移です。".PHP_EOL);
